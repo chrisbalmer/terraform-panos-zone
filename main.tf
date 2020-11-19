4f5ec09c996b1f15866fd212c8ab4dcc28868720
Chris Balmer <git@chrisbalmer.io>
2020-11-19 16:40:05 -0500

    Added management profile and log setting

--- main.tf
+++ main.tf
@@ -3,11 +3,12 @@ locals {
 }
 
 resource "panos_ethernet_interface" "gateway" {
-  name       = local.zone.interface
-  vsys       = local.zone.vsys
-  mode       = local.zone.mode
-  static_ips = local.zone.ips
-  comment    = local.zone.comment
+  name               = local.zone.interface
+  vsys               = local.zone.vsys
+  mode               = local.zone.mode
+  static_ips         = local.zone.ips
+  management_profile = local.zone.management_profile
+  comment            = local.zone.comment
 }
 
 resource "panos_virtual_router_entry" "interface_to_router" {
@@ -19,6 +20,7 @@ resource "panos_zone" "zone" {
   name           = local.zone.name
   mode           = local.zone.mode
   enable_user_id = local.zone.user_id
+  log_setting    = local.zone.log_setting
 }
 
 resource "panos_zone_entry" "interface_to_zone" {
--- variables.tf
+++ variables.tf
@@ -4,24 +4,28 @@ variable "zone" {
 
 variable "zone_defaults" {
   type = object({
-    interface = string
-    ips       = list(string)
-    name      = string
-    comment   = string
-    mode      = string
-    vsys      = string
-    router    = string
-    user_id   = bool
+    interface          = string
+    ips                = list(string)
+    name               = string
+    comment            = string
+    log_setting        = string
+    management_profile = string
+    mode               = string
+    vsys               = string
+    router             = string
+    user_id            = bool
   })
 
   default = {
-    interface = ""
-    ips       = []
-    name      = ""
-    comment   = ""
-    mode      = "layer3"
-    vsys      = "vsys1"
-    router    = "default"
-    user_id   = true
+    interface          = ""
+    ips                = []
+    name               = ""
+    comment            = ""
+    log_setting        = null
+    management_profile = null
+    mode               = "layer3"
+    vsys               = "vsys1"
+    router             = "default"
+    user_id            = true
   }
 }
